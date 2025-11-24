bce3e350a17476c66783310378e15b34e92c9ba0
Karan Minchekar <144414843+mkaran02@users.noreply.github.com>
2025-11-24 14:24:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//event handling for about page
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("About page loaded");
+});
+
+let Thank = "Thank you for your interest in Dream Restaurant!";
